d89d0ab6e823999d0ea5ae0af7cb7ae56445c09a
Kirby Munson <kmunson@g.clemson.edu>
2018-01-29 14:05:39 -0500

    first commit

---
+++ phil.js
@@ -0,0 +1,13 @@
+
+(function(){
+  "usestrict";
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1100);
+});
+
+}());
